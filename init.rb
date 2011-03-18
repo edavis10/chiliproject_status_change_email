0e627c76e48e8c23853a2298cb8cbe365a2be4be
Eric Davis <edavis@littlestreamsoftware.com>
2011-03-18 14:12:05 -0700

    Generate plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_status_change_email do
+  name 'Redmine Status Change Email plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
