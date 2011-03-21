03924871ea034fe35501615cf6c120d14b6d4571
Eric Davis <edavis@littlestreamsoftware.com>
2011-03-21 08:58:24 -0700

    Add tests and code for IssueStatusChange#extra_content_for

--- app/models/issue_status_change.rb
+++ app/models/issue_status_change.rb
@@ -10,4 +10,35 @@ class IssueStatusChange < ActiveRecord::Base
       "watcher" => "0"
     }
   end
+
+  # Finds the extra_content based on the issue's current status and
+  # if the user is supposed to see anything.
+  def self.extra_content_for(issue, user)
+    return nil unless issue.status.present?
+
+    issue_status_change = issue.status.issue_status_change
+    return nil unless issue_status_change.present?
+
+    if issue_status_change.author? && issue.author == user
+      return issue_status_change.extra_content
+    end
+
+    if issue_status_change.assigned_to? && issue.assigned_to == user
+      return issue_status_change.extra_content
+    end
+
+    if issue_status_change.watcher? && issue.watched_by?(user)
+      return issue_status_change.extra_content
+    end
+
+    return nil
+  end
+
+  # object_daddy defaults for testing
+  if Rails.env.test?
+    generator_for :author => false
+    generator_for :watcher => false
+    generator_for :assigned_to => false
+    generator_for :extra_content => 'Generated'
+  end
 end
--- test/unit/issue_status_change_test.rb
+++ test/unit/issue_status_change_test.rb
@@ -4,19 +4,77 @@ class IssueStatusChangeTest < ActiveSupport::TestCase
   should_belong_to(:issue_status)
   
   context "#extra_content_for(issue, user)" do
-    context "for the author" do
-      should "return the content if the author? option is true"
-      should "return nothing if the author? option is false"
+    setup do
+      @status = IssueStatus.generate!(:name => "New")
+      @author = User.generate!
+      @watcher = User.generate!
+      @assigned_to = User.generate!
+
+      @project = Project.generate!
+      @issue = Issue.generate_for_project!(@project,
+                                           :status => @status,
+                                           :author => @author,
+                                           :assigned_to => @assigned_to)
+      @issue.add_watcher(@watcher)
+    end
+    
+    context "for the issue author" do
+      should "return the extra_content if the IssueStatusChange#author? option is true" do
+        IssueStatusChange.generate!(:issue_status => @status,
+                                    :extra_content => 'test',
+                                    :author => true)
+        
+        assert_equal 'test', IssueStatusChange.extra_content_for(@issue, @author)
+      end
+      
+      
+      should "return nothing if the IssueStatusChange#author? option is false" do
+        IssueStatusChange.generate!(:issue_status => @status,
+                                    :extra_content => 'test',
+                                    :author => false)
+        
+        assert_equal nil, IssueStatusChange.extra_content_for(@issue, @author)
+
+      end
+      
     end
 
-    context "for a watcher" do
-      should "return the content if the watcher? option is true"
-      should "return nothing if the watcher? option is false"
+    context "for an issue watcher" do
+      should "return the extra_content if the IssueStatusChange#watcher? option is true" do
+        IssueStatusChange.generate!(:issue_status => @status,
+                                    :extra_content => 'test',
+                                    :watcher => true)
+        
+        assert_equal 'test', IssueStatusChange.extra_content_for(@issue, @watcher)
+      end
+      
+      should "return nothing if the IssueStatusChange#watcher? option is false" do
+        IssueStatusChange.generate!(:issue_status => @status,
+                                    :extra_content => 'test',
+                                    :watcher => false)
+        
+        assert_equal nil, IssueStatusChange.extra_content_for(@issue, @watcher)
+      end
+      
     end
 
-    context "for the assignee" do
-      should "return the content if the assigned_to? option is true"
-      should "return nothing if the assigned_to? option is false"
+    context "for the issue assignee" do
+      should "return the extra_content if the IssueStatusChange#assigned_to? option is true" do
+        IssueStatusChange.generate!(:issue_status => @status,
+                                    :extra_content => 'test',
+                                    :assigned_to => true)
+        
+        assert_equal 'test', IssueStatusChange.extra_content_for(@issue, @assigned_to)
+      end
+      
+      should "return nothing if the IssueStatusChange#assigned_to? option is false" do
+        IssueStatusChange.generate!(:issue_status => @status,
+                                    :extra_content => 'test',
+                                    :assigned_to => false)
+        
+        assert_equal nil, IssueStatusChange.extra_content_for(@issue, @assigned_to)
+      end
+      
     end
   end
   
